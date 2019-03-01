8361a6dab017e351448670e4ea476aaa8573c12d
Derek Houck <derek@derekhouck.com>
2019-03-01 11:51:10 -0800

    Implement Queue class

--- queue.js
+++ queue.js
@@ -1,20 +1,75 @@
 'use strict';
 
 class _Node {
-  constructor(data, next) {
-    
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
   }
 }
 
 class Queue {
   constructor() {
-    
+    this.first = null;
+    this.last = null;
+  }
+
+  enqueue(data) {
+    const node = new _Node(data);
+
+    if (this.first === null) {
+      this.first = node;
+    }
+
+    if (this.last) {
+      node.next = this.last;
+      this.last.prev = node;
+    }
+
+    this.last = node;
+  }
+
+  dequeue() {
+    if (this.first === null) {
+      return;
+    }
+
+    const node = this.first;
+    this.first = node.prev;
+
+    if (node === this.last) {
+      this.last = null;
+    }
+
+    return node.value;
   }
 }
+
+const peek = queue => queue.first !== null ? queue.first.value : null;
+
+const display = queue => {
+  let currNode = queue.first;
+  while (currNode !== null) {
+    console.log(currNode.value);
+    currNode = currNode.prev;
+  }
+};
 
 const starTrekDrills = () => {
   const starTrekQ = new Queue();
 
+  starTrekQ.enqueue('Kirk');
+  starTrekQ.enqueue('Spock');
+  starTrekQ.enqueue('Uhura');
+  starTrekQ.enqueue('Sulu');
+  starTrekQ.enqueue('Checkov');
+
+  // console.log(peek(starTrekQ));
+  // display(starTrekQ);
+
+  starTrekQ.dequeue();
+  starTrekQ.dequeue();
+  display(starTrekQ);
 };
 
 starTrekDrills();
