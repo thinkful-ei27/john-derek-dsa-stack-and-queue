6192f5d50fe913e4be97bda11fe60f37bcdb3601
Derek Houck <derek@derekhouck.com>
2019-03-01 08:24:52 -0800

    palindromes drill

--- stack.js
+++ stack.js
@@ -33,15 +33,45 @@ const peek = stack => {
   return stack.top.data;
 };
 
-const main = () => {
-  const starTrek = new Stack();
-
-  starTrek.push('Kirk');
-  starTrek.push('Spock');
-  starTrek.push('McCoy');
-  starTrek.push('Scotty');
-
-  console.log(peek(starTrek));
+const display = stack => {
+  let currNode = stack.top;
+  while (currNode !== null) {
+    console.log(currNode.data);
+    currNode = currNode.next;
+  }
 };
 
-main();
+// const starTrekDrills = () => {
+//   const starTrek = new Stack();
+
+//   starTrek.push('Kirk');
+//   starTrek.push('Spock');
+//   starTrek.push('McCoy');
+//   starTrek.push('Scotty');
+
+//   // console.log(peek(starTrek));
+//   starTrek.pop();
+//   starTrek.pop();
+//   // display(starTrek);
+// };
+
+// starTrekDrills();
+
+// Check for palindromes using stack
+
+function is_palindrome(s) {
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  const regStack = new Stack();
+  for (let i = 0; i < s.length; i++) {
+    regStack.push(s[i]);
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (regStack.pop() !== s[i]) { return false; }
+  }
+  return true;
+}
+
+console.log(is_palindrome('dad'));
+console.log(is_palindrome('A man, a plan, a canal: Panama'));
+console.log(is_palindrome('1001'));
+console.log(is_palindrome('Tauhida'));
